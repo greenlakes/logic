847b29a7d8458e3a10ddce78625852aeacd0faaa
Greenlakes <greenlakes2005@gmail.com>
2017-03-31 11:52:39 +0000

    full quiz complete

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -1,23 +1,33 @@
 class PlaysController < ApplicationController
+  
+  before_action :authenticate_user!
+  
   def index
     @quizzes = Quiz.all
   end
 
   def show
-    @quiz = Quiz.find(params[:id])
+    @quiz = Quiz.find(params[:id]) 
   end
 
 
   def check_results
-    quiz = Quiz.find(params[:quiz][:id])
+    quiz = Quiz.find(params[:quiz][:id]) 
     count = 0
-    questions_with_answers = params[:quiz][:questions]
     
-    quiz.questions.map do |question|
-      correct_answer_id = question.answers.find_by(correct: true).try(:id) || 0
-      puts correct_answer_id
-      puts questions_with_answers["#{question.id}"] 
-      count += 1 if questions_with_answers["#{question.id}"].to_i == correct_answer_id
+    if questions_with_answers = params[:quiz][:questions]
+      quiz.questions.each do |question|
+        correct_answer = question.answers.find_by(correct: true).try(:id)
+        user_answer    = questions_with_answers[question.id.to_s].to_i
+        count += 1 if user_answer == correct_answer
+      end
+      
+      # Вот тут нужно создавать Play для текущего пользователя
+      # с записью количества правильных ответов – current_user.plays.create(...)
+
+
+    @play = current_user.plays.create(quiz_id: params[:quiz][:id].to_i, points: count.to_i)
+    
     end
     
     redirect_to :back, notice: "Правильных ответов: #{count}"
